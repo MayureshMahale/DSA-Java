b6f9319280c8c480bf7685115d60abb4254af4e1
MayureshMahale <mayureshmahale1010@gmail.com>
2026-08-08 21:45:39 +0530

    Added TUF Pattern6

---
+++ TUF/Patterns/Pattern6.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void pattern6(int n) {
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-i;j++){
+                System.out.print(j+1);
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void main(String[] args){
+        int n=4;
+        Solution sol = new Solution();
+
+        sol.pattern6(n);
+    }
+}
