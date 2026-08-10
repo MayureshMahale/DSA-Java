1fd1ed7ad53e3e8bae40ddd0d44a6883b6c189c1
MayureshMahale <mayureshmahale1010@gmail.com>
2026-08-10 19:48:29 +0530

    Added TUF Pattern16

---
+++ TUF/Patterns/Pattern16.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void pattern16(int n) {
+        char ch='A';
+        for(int i=0; i<n; i++){
+            for(int j=0; j<=i; j++){
+                System.out.print(ch);
+                
+            }
+            ch=(char)(ch+1);
+            System.out.println();
+        }
+    }
+}
