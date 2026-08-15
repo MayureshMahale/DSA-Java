2026196b30b861ad3dd7c1dcf20b705e9eab7c2a
MayureshMahale <mayureshmahale1010@gmail.com>
2026-08-15 07:05:07 +0530

    Count number of odd digits in a number

--- TUF/Basic Math/Count_number_of_odd_digits_in_a_number.java
+++ TUF/Basic Math/Count_number_of_odd_digits_in_a_number.java
@@ -1,16 +1,20 @@
 class Solution {
-    public int countDigit(int n) {
+    public int countOddDigit(int n) {
         if(n == 0) {
+            if(n%2 !=0)
             return 1;
         }
 
         int count = 0;
 
         while(n != 0) {
-            count++;
+            if(n%2!=0){
+                count++;
+            }
             n = n / 10;
         }
 
         return count;
+    
     }
 }
