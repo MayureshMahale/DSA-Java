2da9961876f00443454b62fb3183ff4d0ed19af4
MayureshMahale <mayureshmahale1010@gmail.com>
2026-08-19 21:02:30 +0530

    LCM of two numbers

---
+++ TUF/Basic Math/LCM_of_two_numbers.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int LCM(int n1, int n2) {
+        int lcm;
+        int n=Math.max(n1,n2);
+        int i=1;
+        while(true){
+            int mul=n*i;
+            if(mul%n1==0 && mul%n2==0){
+                lcm=mul;
+                break;
+            }
+            i++;
+        }
+        return lcm;
+    }
+}
