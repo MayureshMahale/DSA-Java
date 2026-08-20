a503fece9fc722b48943c601b75cbc3877b88273
MayureshMahale <mayureshmahale1010@gmail.com>
2026-08-20 21:20:15 +0530

    283. Move Zeroes

--- LeetCode/Array/283_Move_Zeroes.java
+++ LeetCode/Array/283_Move_Zeroes.java
@@ -5,7 +5,7 @@ class Solution {
         for(int i=0;i<n;i++){
             if(nums[i]!=0){
                 int temp=nums[i];
-                nums[i]=nums[j];
+                nums[i]=nums[j];    
                 nums[j]=temp;
                 j++;
             }
