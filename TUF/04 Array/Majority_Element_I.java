2936870f1ce88dc03d58d6199701d9a4731bd057
Mayuresh Mahale <mayureshmahale1010@gmail.com>
2026-08-31 19:00:08 +0530

    Majority Element-I

---
+++ TUF/04 Array/Majority_Element_I.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+       for (int i=0;i<nums.length;i++){
+            int cnt=0;
+            for(int j=i;j<nums.length;j++){
+                if(nums[i]==nums[j]){
+                    cnt++;
+                }  
+            }    
+            if(cnt>(nums.length/2)){
+                    return nums[i];
+            }
+        } 
+       return -1;
+    }
+}
