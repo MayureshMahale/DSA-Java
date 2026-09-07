48b02f863eeb8464b35918496ba0af60e78778fa
Mayuresh Mahale <mayureshmahale1010@gmail.com>
2026-09-07 19:03:42 +0530

    Solution

--- 01  TUF/04 Array/Rotate_matrix_by_90_degrees.java
+++ 01  TUF/04 Array/Rotate_matrix_by_90_degrees.java
@@ -1,14 +1,26 @@
 class Solution {
+    
     public void rotateMatrix(int[][] matrix) {
-        int n=matrix.length;
-        int[][] rotated = new int[n][n];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                rotated[j][n-i-1]=matrix[i][j];
+        int n = matrix.length;
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+                
             }
         }
+        
         for (int i = 0; i < n; i++) {
-            System.arraycopy(rotated[i], 0, matrix[i], 0, n);
+            for (int j = 0; j < n / 2; j++) {
+                
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - 1 - j];
+                matrix[i][n - 1 - j] = temp;
+                
+            }
         }
     }
 }
